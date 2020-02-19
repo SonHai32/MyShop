c55867805df720bb5e3450c5510815e0e5ef6f6d
lamsonhai1379 <lamsonhai1379@gmail.com>
2020-02-19 17:16:27 +0700

    🌟 public upload folder

--- server/src/index.js
+++ server/src/index.js
@@ -27,6 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(morgan('common'));
 app.use(helmet());
+app.use('/uploads', express.static('uploads'))
 app.use(cors({
     origin: process.env.CORS_ORIGIN
 }));
