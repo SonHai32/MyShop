8be2c874b9173248a05aee4e062f80d3cad6ae42
lamsonhai1379 <lamsonhai1379@gmail.com>
2020-02-22 15:10:10 +0700

    fixed update null Object

--- server/src/api/Users.js
+++ server/src/api/Users.js
@@ -36,7 +36,7 @@ router.post('/Create', (req, res, next) =>{
 })
 
 const updateUser = (ID, user, res, next) =>{
-    try{
+    try{ 
         users.updateOne({"_id": ID}, {$set: user}, (err, result) =>{
             if(err){
                 res.json({
@@ -61,17 +61,24 @@ const updateUser = (ID, user, res, next) =>{
 router.put('/Update/:id', (req, res, next) =>{
         const userId = req.params.id
         let user = req.body;
-        if(user.password){
-            bcrypt.hash(user.password, 10, (err, hashPassword) =>{
-                user.password = hashPassword;
-                updateUser(userId, user, res, next)
-                
-            })
-        }else{
-
-            updateUser(userId, user, res, next)
-           
-        } 
+        users.findById(userId, (err, result) =>{
+           if(!err && result){
+                if(user.password){
+                    bcrypt.hash(user.password, 10, (err, hashPassword) =>{
+                        user.password = hashPassword;
+                        updateUser(userId, user, res, next) 
+                })
+                }else{
+                    updateUser(userId, user, res, next)
+                }
+            }else{
+                res.status(404)
+                res.json({
+                    Message: "ERROR: NOT FOUND"
+                })
+            } 
+        })
+         
 })
 
 
